2a9813951ecbe6a5851cda974fc29be7dad18d20
Artem <artem.tolmachev.work@gmail.com>
2023-07-26 12:22:41 +0300

    Add code optimization

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot
 from telebot import types
 import roll_dice_system as DICE
-import random
 
 bot = telebot.TeleBot("6066347084:AAGd4EF5XIdluOLxFeXla-erVSziyDig8lo")
 
@@ -14,22 +13,16 @@ def bot_start(message):
 @bot.message_handler(commands=["give_dice"])
 def give_dice(message: types.Message):
     markup = types.InlineKeyboardMarkup()
-    btnDice = types.InlineKeyboardButton(text="Give me a Dice!", callback_data="1")
-    markup.add(btnDice)
+    btn_dice = types.InlineKeyboardButton(text="Make Move", callback_data="1")
+    markup.add(btn_dice)
     bot.send_message(message.chat.id, "Ok, I give you dices!", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def bot_roll_dice(call):
     if call.data == "1":
-        n1 = int(random.uniform(1, 7))
-        n2 = int(random.uniform(1, 7))
-        result = DICE.call(n1, n2, "dice1.jpg", "dice2.jpg")
-        result.show()
-        bot.send_photo(call.message.chat.id, result)
+        bot.send_photo(call.message.chat.id, DICE.call(0, 0, "dice1.jpg", "dice2.jpg"))
         bot.send_message(call.message.chat.id, "You rolled dice!")
-    else:
-        pass
 
 
 bot.polling(none_stop=True)
--- roll_dice_system.py
+++ roll_dice_system.py
@@ -1,7 +1,5 @@
 from PIL import Image
 import random
-n1 = int()
-n2 = int()
 img = Image.open("DICE_EMPTY.jpg")
 img.load()
 img2 = Image.open("DICE_EMPTY.jpg")
@@ -64,7 +62,6 @@ def call(number1, number2, savename1, savename2):
     print(number1, number2)
     dice1 = gen_dice(number1, img, savename1)
     dice2 = gen_dice(number2, img2, savename2)
-    # print(number1, number2)
     result = Image.new('RGB', (2160, 1440))
     result.load()
     dice1.load()
@@ -72,7 +69,6 @@ def call(number1, number2, savename1, savename2):
     result.paste(dice1, (0, 0))
     result.paste(dice2, (1080, 0))
     result.save("dice_result.jpg")
-    result.show()
     move_points = number1 + number2
     print("You got", move_points, "points!")
     return result
